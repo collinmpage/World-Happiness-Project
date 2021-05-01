a49ad99f292d739d82cb19aa6296fe1582a77066
Haley Peterson <haleyapeterson@gmail.com>
2021-05-01 11:53:02 -0500

    update

--- yearlyPlot.js
+++ yearlyPlot.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 900 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -13,11 +13,19 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.json("", function(data) {
+d3.csv("Cleaned_Data/cleaned_all.csv", function(data) {
 
     // List of groups (here I have one group per column)
-    var allGroup = d3.map(data, function(d){return(d.name)}).keys()
+    var allGroup = d3.map(data, function(d){return(d.country_or_region)}).keys()
 
+    // add the country to the name
+    d3.select("#countryname")
+      .selectAll('text')
+      .data(allGroup)
+      .enter()
+      .append('div')
+      .text(d => d.country_or_region)
+      
     // add the options to the button
     d3.select("#selectButton")
       .selectAll('myOptions')
@@ -27,22 +35,17 @@ d3.json("", function(data) {
       .text(function (d) { return d; }) // text showed in the menu
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
-    // A color scale: one color for each group
-    var myColor = d3.scaleOrdinal()
-      .domain(allGroup)
-      .range(d3.schemeSet2);
-
     // Add X axis --> it is a date format
     var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) { return d.year; }))
+      .domain([2015, 2019])
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(7));
+      .call(d3.axisBottom(x).ticks(5));
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.n; })])
+      .domain([0, d3.max(data, function(d) { return +d.happiness_score; })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -51,31 +54,91 @@ d3.json("", function(data) {
     var line = svg
       .append('g')
       .append("path")
-        .datum(data.filter(function(d){return d.name==allGroup[0]}))
+        .datum(data.filter(function(d){return d.country_or_region==allGroup[0]}))
         .attr("d", d3.line()
-          .x(function(d) { return x(d.year) })
-          .y(function(d) { return y(+d.n) })
+          .x(function(d) { return x(+d.year) })
+          .y(function(d) { return y(+d.happiness_score) })
         )
-        .attr("stroke", function(d){ return myColor("valueA") })
+        .attr("stroke", "black")
         .style("stroke-width", 4)
         .style("fill", "none")
 
+    // Initialize dots with group a
+    var dot = svg
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+        .attr("cx", function(d) { return x(+d.year) })
+        .attr("cy", function(d) { return y(+d.happiness_score) })
+        .attr("r", 7)
+        .style("fill", "#69b3a2")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+
+    // Add tooltip
+    // create a tooltip
+    var Tooltip = d3.select("#my_dataviz")
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+
+      // Three function that change the tooltip when user hover / move / leave a cell
+      var mouseover = function(d) {
+        Tooltip
+          .style("opacity", 1)
+      }
+      var mousemove = function(d) {
+        Tooltip
+          .html(`Year: ${d.year}<br>Rank: ${d.overall_rank}<br>Happiness Score: ${d.happiness_score}`)
+          .style("left", (d3.mouse(this)[0]+70) + "px")
+          .style("top", (d3.mouse(this)[1]) + "px")
+      }
+      var mouseleave = function(d) {
+        Tooltip
+          .style("opacity", 0)
+      }
+    
     // A function that update the chart
     function update(selectedGroup) {
 
       // Create new data with the selection?
-      var dataFilter = data.filter(function(d){return d.name==selectedGroup})
+      var dataFilter = data.filter(function(d){return d.country_or_region==selectedGroup})
 
       // Give these new data to update line
       line
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(d.year) })
-            .y(function(d) { return y(+d.n) })
-          )
-          .attr("stroke", function(d){ return myColor(selectedGroup) })
+        .datum(dataFilter)
+        .transition()
+        .duration(1000)
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.year) })
+          .y(function(d) { return y(+d.happiness_score) })
+        )
+        .attr("stroke", "black")
+      
+      // Give these new data to update dot
+      dot
+        .data(dataFilter)
+        .transition()
+        .duration(1000)
+          .attr("cx", function(d) { return x(+d.year) })
+          .attr("cy", function(d) { return y(+d.happiness_score) })
+          .on("mouseover", mouseover)
+          .on("mousemove", mousemove)
+          .on("mouseleave", mouseleave)
+      
+      // Give these new data to update tooltip
+      mousemove
+        .data(dataFilter)
+        .transition()
+        .duration(1000)
+          .html(`Year: ${d.year}<br>Rank: ${d.overall_rank}<br>Happiness Score: ${d.happiness_score}`)
     }
 
     // When the button is changed, run the updateChart function
@@ -85,6 +148,7 @@ d3.json("", function(data) {
         // run the updateChart function with this selected option
         update(selectedOption)
     })
+
 
 })
 
