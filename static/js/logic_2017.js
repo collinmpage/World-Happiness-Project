ea12321e92282adf9adead802136de31f165569a
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-05-05 20:52:02 -0500

    Update logic_2017.js

--- static/js/logic_2017.js
+++ static/js/logic_2017.js
@@ -4,6 +4,9 @@ var myMap = L.map("map", {
   zoom: 3
 });
 
+const API_KEY = "pk.eyJ1IjoiY29sbGlubXBhZ2UiLCJhIjoiY2tucDh4aG9wMWRwbzJ1cHUzbndudG9nayJ9.ZT_9--wXlrj4sWpWepLTiQ"
+
+
 // Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
