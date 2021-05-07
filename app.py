a6aed49ea110f5843da5a3c012f6127a7ab8d800
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-05-06 19:19:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -99,79 +99,36 @@ def drew():
 def test():
     return render_template("test.html")
 
+#################################################
+# test
+#################################################
 
-
-
-
-
-
-
-
-
-
-
-# import numpy as np
-
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func
-
-# from flask import Flask, jsonify, render_template, redirect
+@app.route("/2017")
+def test2():
+    return render_template("2017_map.html")
 
 # #################################################
-# # Database Setup
-# #################################################
-# engine = create_engine("sqlite:///world_happiness.sqlite")
-
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(engine, reflect=True)
-
-# # Save reference to the table
-# table_2015 = Base.classes.data_2015
-# table_2016 = Base.classes.data_2016
-# table_2017 = Base.classes.data_2017
-# table_2018 = Base.classes.data_2018
-# table_2019 = Base.classes.data_2019
-
-# #################################################
-# # Flask Setup
-# #################################################
-# app = Flask(__name__)
-
-
-# #################################################
-# # Flask Routes
+# # test
 # #################################################
 
-# # Route to render index.html template using data from Mongo
-# @app.route("/")
-# def home():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/2018")
+def test3():
+    return render_template("2018_map.html")
 
-#     """Return a list of data"""
-#     # Query all tables data
-#     results_2015 = session.query(table_2015.["Overall Rank"], table_2015.[""] ).all()
-#     results_2016 = session.query(table_2016.column_wanted).all()
-#     results_2017 = session.query(table_2017.column_wanted).all()
-#     results_2018 = session.query(table_2018.column_wanted).all()
-#     results_2019 = session.query(table_2019.column_wanted).all()
+# #################################################
+# # test
+# #################################################
 
-#     session.close()
- 
-#     # Create a dictionary from the row data and append to a list of data_2015
-#     data_2015 = []
-#     for column1, column2, column3, etc in results:
-#         dict_2015 = {}
-#         dict_2015["column1"] = column1
-#         dict_2015["column2"] = column2
-#         dict_2015["column3"] = column3
-#         data_2015.append(dict_2015)
+@app.route("/2019")
+def test4():
+    return render_template("2019_map.html")
 
-#     return render_template("index.html", jsonify(data_2015))
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
